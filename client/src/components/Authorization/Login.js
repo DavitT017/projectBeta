c05d440d2bb6aea12abd2e8cbd5879b04059f302
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-04-19 19:02:45 +0400

    Just come outside for the night (yeah)

---
+++ client/src/components/Authorization/Login.js
@@ -0,0 +1,97 @@
+import { useFormik } from "formik"
+import React from "react"
+import * as yup from "yup"
+
+const initialValues = {
+    emailOrUsername: "",
+    password: "",
+}
+
+const validationSchema = yup.object().shape({
+    emailOrUsername: yup
+        .string()
+        .required("Required")
+        .matches(
+            /(?=.{3,50}$)/,
+            "Email or username must be between 3 and 50 characters"
+        )
+        .test("is-valid", "Invalid username or email", (value) => {
+            const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+            const usernameRegex = /^[a-zA-Z0-9_]+$/
+
+            return emailRegex.test(value) || usernameRegex.test(value)
+        }),
+    password: yup
+        .string()
+        .matches(
+            /(?=.*[A-Z])/,
+            "Password must contain at least one uppercase letter"
+        )
+        .matches(/(?=.{6,20}$)/, "Password must be between 6 and 20 characters")
+        .matches(
+            /[ -/:-@[-`{-~]/,
+            "Password must contain at least one special character"
+        )
+        .matches(/(?=.*[0-9])/, "Password must contain at least one digit")
+        .required("Required"),
+})
+
+const onSubmit = (values, onSubmitProps) => {
+    console.log("Form values", values)
+    onSubmitProps.resetForm()
+}
+
+const Login = () => {
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit,
+        validateOnBlur: true,
+        validateOnChange: true,
+    })
+
+    return (
+        <div>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <div>
+                        <label htmlFor="emailOrUsername">
+                            Email or Username
+                        </label>
+                    </div>
+                    <br />
+                    <input
+                        type="text"
+                        name="emailOrUsername"
+                        id="emailOrUsername"
+                        {...formik.getFieldProps("emailOrUsername")}
+                    />
+                    {formik.touched.emailOrUsername &&
+                    formik.errors.emailOrUsername ? (
+                        <div>{formik.errors.emailOrUsername}</div>
+                    ) : null}
+                </div>
+                <div>
+                    <div>
+                        <label htmlFor="password">Password</label>
+                    </div>
+                    <br />
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        {...formik.getFieldProps("password")}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                        <div>{formik.errors.password}</div>
+                    ) : null}
+                </div>
+                <button type="submit" disabled={!formik.isValid}>
+                    LOG IN
+                </button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
