eab9c7eb683a6558354d709fa594a3baa5a47b79
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-04-19 19:17:26 +0400

    Take your time, get your light (yeah)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,27 @@
 import "./App.css"
+import { Routes, Route } from "react-router-dom"
+import Login from "./components/Authorization/Login"
+import Register from "./components/Authorization/Register"
+import Bookmarks from "./components/Bookmarks/Bookmarks"
+import Comments from "./components/Comments/Comments"
+import Forum from "./components/Forum/Forum"
+import Genres from "./components/Genres/Genres"
+import Rating from "./components/Rating/Rating"
 
 function App() {
-    return <div className="App">Hello, World!</div>
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/bookmarks" element={<Bookmarks />} />
+                <Route path="/comments" element={<Comments />} />
+                <Route path="/forum" element={<Forum />} />
+                <Route path="/genres" element={<Genres />} />
+                <Route path="/rating" element={<Rating />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
