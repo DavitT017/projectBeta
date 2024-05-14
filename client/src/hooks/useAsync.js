05dc4c64cd8e727891021184dbcaba85ac3db0fb
Aram Arakelyan <aram777arakelyan@gmail.com>
2024-05-14 14:18:57 +0400

    Going places where you don't belong

--- client/src/hooks/useAsync.js
+++ client/src/hooks/useAsync.js
@@ -39,6 +39,7 @@ export function useAsyncInternal(
             .finally(() => {
                 setLoading(false)
             })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, dependencies)
 
     return { loading, error, value, execute }
