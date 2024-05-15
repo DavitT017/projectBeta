7b9a83b6c6a254372b337ea1b95353f8612b5908
DavitT017 <tadevosyandavo888@gmail.com>
2024-05-15 15:51:36 +0400

    Late-night when you need my love

--- client/src/components/Forum/Thread.js
+++ client/src/components/Forum/Thread.js
@@ -1,48 +1,83 @@
-import React from "react"
-import { useThread, handleRequestError } from "../../context/ThreadContext"
-import CommentsList from "./ThreadCommentsList"
-import CommentForm from "../Comments/CommentForm"
+import React, { useState } from "react"
+import { handleRequestError, useThread } from "../../context/ThreadContext"
 import { useAsyncFn } from "../../hooks/useAsync"
 import { createComment } from "../../services/threadComments"
+import { deleteThread, updateThread } from "../../services/threads"
+import CommentForm from "../Comments/CommentForm"
+import CommentsList from "./ThreadCommentsList"
 
 function Thread() {
-    const { thread, rootComments, createLocalComment } = useThread()
+	const {
+		thread,
+		rootComments,
+		createLocalComment,
+		deleteLocalThread,
+		updateLocalThread,
+	} = useThread()
+	const {
+		loading: updateLoading,
+		error: updateError,
+		execute: updateThreadFn,
+	} = useAsyncFn(updateThread)
+	const {
+		loading: deleteLoading,
+		error: deleteError,
+		execute: deleteThreadFn,
+	} = useAsyncFn(deleteThread)
 
-    const {
-        loading,
-        error,
-        execute: createCommentFn,
-    } = useAsyncFn(createComment)
+	const [isEditing, setIsEditing] = useState(false)
 
-    function onCommentCreate(message) {
-        return createCommentFn({ thread_id: thread.thread_id, message })
-            .then(createLocalComment)
-            .catch(handleRequestError)
-    }
-    return (
-        <div>
-            <div style={{ border: "1px solid" }}>
-                <h1>{thread.title}</h1>
-                <p>{thread.description}</p>
-                <button>Edit</button>
-            </div>
-            <div>
-                <h3>Comments</h3>
-                <div>
-                    <CommentForm
-                        loading={loading}
-                        error={error}
-                        handleSubmit={onCommentCreate}
-                    />
-                    {rootComments && rootComments.length > 0 ? (
-                        <div>
-                            <CommentsList comments={rootComments} />
-                        </div>
-                    ) : null}
-                </div>
-            </div>
-        </div>
-    )
+	const {
+		loading,
+		error,
+		execute: createCommentFn,
+	} = useAsyncFn(createComment)
+
+	function onCommentCreate(message) {
+		return createCommentFn({ thread_id: thread.thread_id, message })
+			.then(createLocalComment)
+			.catch(handleRequestError)
+	}
+
+	function onThreadUpdate(values) {
+		return updateThreadFn({ ...values, thread_id: thread.thread_id })
+			.then((updatedThread) => {
+				updateLocalThread(updatedThread)
+				setIsEditing(false)
+			})
+			.catch(handleRequestError)
+	}
+
+	function onThreadDelete() {
+		return deleteThreadFn({ thread_id: thread.thread_id })
+			.then(() => deleteLocalThread(thread.thread_id))
+			.catch(handleRequestError)
+	}
+
+	return (
+		<div>
+			<div style={{ border: "1px solid" }}>
+				<h1>{thread.title}</h1>
+				<p>{thread.description}</p>
+				<button>Edit</button>
+			</div>
+			<div>
+				<h3>Comments</h3>
+				<div>
+					<CommentForm
+						loading={loading}
+						error={error}
+						handleSubmit={onCommentCreate}
+					/>
+					{rootComments && rootComments.length > 0 ? (
+						<div>
+							<CommentsList comments={rootComments} />
+						</div>
+					) : null}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Thread
--- client/src/services/threads.js
+++ client/src/services/threads.js
@@ -1,16 +1,29 @@
 import { makeRequest } from "./makeRequest"
 
 export function getThreads() {
-    return makeRequest("/threads")
+	return makeRequest("/threads")
 }
 
 export function getAThread(thread_id) {
-    return makeRequest(`/threads/${thread_id}`)
+	return makeRequest(`/threads/${thread_id}`)
 }
 
 export function createThread({ title, description, thread_type }) {
-    return makeRequest(`/threads`, {
-        method: "POST",
-        data: { title, description, thread_type },
-    })
+	return makeRequest(`/threads`, {
+		method: "POST",
+		data: { title, description, thread_type },
+	})
+}
+
+export function updateThread({ thread_id, title, description, thread_type }) {
+	return makeRequest(`/threads/${thread_id}`, {
+		method: "PUT",
+		data: { title, description, thread_type },
+	})
+}
+
+export function deleteThread(thread_id) {
+	return makeRequest(`/threads/${thread_id}`, {
+		method: "DELETE",
+	})
 }
